ab6f301b9c14f69d4c5695b866376902046669b8
Blaize M Kaye <blaize.kaye@gmail.com>
2023-11-07 03:58:58 +1300

    Fixes error handling in remote writer

--- gatherers/insights-remote-writer.go
+++ gatherers/insights-remote-writer.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
-
 	"github.com/spf13/viper"
 	insightRemoteLib "github.com/uselagoon/insights-remote-lib"
+	"log"
+	"net/http"
 )
 
 func WriteFactsToInsightsRemote(token string, facts []GatheredFact) error {
@@ -29,7 +28,10 @@ func WriteFactsToInsightsRemote(token string, facts []GatheredFact) error {
 		insightsRemoteFacts.Facts = append(insightsRemoteFacts.Facts, f)
 	}
 
-	bodyString, _ := json.Marshal(insightsRemoteFacts)
+	bodyString, err := json.Marshal(insightsRemoteFacts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 	fmt.Printf("Sending %v fact(s) to insights core\n", len(facts))
 
@@ -39,9 +41,16 @@ func WriteFactsToInsightsRemote(token string, facts []GatheredFact) error {
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	response, err := client.Do(req)
-	if response.StatusCode != 200 {
-		fmt.Printf("There was an error sending the facts to '%s' : %s\n", serviceEndpoint, response.Body)
-		os.Exit(1)
+
+	if err != nil {
+		return err
 	}
+
+	if response.StatusCode != 200 {
+		log.Fatalf("There was an error sending the facts to '%s' : %s\n", serviceEndpoint, response.Body)
+	}
+
+	defer response.Body.Close()
+
 	return err
 }
